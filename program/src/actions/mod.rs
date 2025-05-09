9b26099d2f3cf5ea2a4a9259bcd77f5be5e0b33b
Tracy Adams <31976538+tracy-codes@users.noreply.github.com>
2025-05-09 15:14:54 -0700

    fix: refactor process_action function for stack usage

--- program/src/actions/mod.rs
+++ program/src/actions/mod.rs
@@ -40,41 +40,75 @@ pub fn process_action(
     let discriminator = unsafe { *(data.get_unchecked(..2).as_ptr() as *const u16) };
     let ix = SwigInstruction::from_primitive(discriminator);
     match ix {
-        SwigInstruction::CreateV1 => {
-            let account_ctx = CreateV1Accounts::context(accounts)?;
-            create_v1(account_ctx, data)
-        },
-        SwigInstruction::SignV1 => {
-            let account_ctx = SignV1Accounts::context(accounts)?;
-            sign_v1(account_ctx, accounts, data, account_classification)
-        },
-        SwigInstruction::AddAuthorityV1 => {
-            let account_ctx = AddAuthorityV1Accounts::context(accounts)?;
-            add_authority_v1(account_ctx, data, accounts)
-        },
-        SwigInstruction::RemoveAuthorityV1 => {
-            let account_ctx = RemoveAuthorityV1Accounts::context(accounts)?;
-            remove_authority_v1(account_ctx, data, accounts)
-        },
-        SwigInstruction::CreateSessionV1 => {
-            let account_ctx = CreateSessionV1Accounts::context(accounts)?;
-            create_session_v1(account_ctx, data, accounts)
-        },
-        SwigInstruction::CreateSubAccountV1 => {
-            let account_ctx = CreateSubAccountV1Accounts::context(accounts)?;
-            create_sub_account_v1(account_ctx, data, accounts)
-        },
+        SwigInstruction::CreateV1 => process_create_v1(accounts, data),
+        SwigInstruction::SignV1 => process_sign_v1(accounts, account_classification, data),
+        SwigInstruction::AddAuthorityV1 => process_add_authority_v1(accounts, data),
+        SwigInstruction::RemoveAuthorityV1 => process_remove_authority_v1(accounts, data),
+        SwigInstruction::CreateSessionV1 => process_create_session_v1(accounts, data),
+        SwigInstruction::CreateSubAccountV1 => process_create_sub_account_v1(accounts, data),
         SwigInstruction::WithdrawFromSubAccountV1 => {
-            let account_ctx = WithdrawFromSubAccountV1Accounts::context(accounts)?;
-            withdraw_from_sub_account_v1(account_ctx, accounts, data, account_classification)
+            process_withdraw_from_sub_account_v1(accounts, account_classification, data)
         },
         SwigInstruction::SubAccountSignV1 => {
-            let account_ctx = SubAccountSignV1Accounts::context(accounts)?;
-            sub_account_sign_v1(account_ctx, accounts, data, account_classification)
+            process_sub_account_sign_v1(accounts, account_classification, data)
         },
-        SwigInstruction::ToggleSubAccountV1 => {
-            let account_ctx = ToggleSubAccountV1Accounts::context(accounts)?;
-            toggle_sub_account_v1(account_ctx, data, accounts)
-        },
+        SwigInstruction::ToggleSubAccountV1 => process_toggle_sub_account_v1(accounts, data),
     }
+}
+
+fn process_create_v1(accounts: &[AccountInfo], data: &[u8]) -> ProgramResult {
+    let account_ctx = CreateV1Accounts::context(accounts)?;
+    create_v1(account_ctx, data)
+}
+
+fn process_sign_v1(
+    accounts: &[AccountInfo],
+    account_classification: &[AccountClassification],
+    data: &[u8],
+) -> ProgramResult {
+    let account_ctx = SignV1Accounts::context(accounts)?;
+    sign_v1(account_ctx, accounts, data, account_classification)
+}
+
+fn process_add_authority_v1(accounts: &[AccountInfo], data: &[u8]) -> ProgramResult {
+    let account_ctx = AddAuthorityV1Accounts::context(accounts)?;
+    add_authority_v1(account_ctx, data, accounts)
+}
+
+fn process_remove_authority_v1(accounts: &[AccountInfo], data: &[u8]) -> ProgramResult {
+    let account_ctx = RemoveAuthorityV1Accounts::context(accounts)?;
+    remove_authority_v1(account_ctx, data, accounts)
+}
+
+fn process_create_session_v1(accounts: &[AccountInfo], data: &[u8]) -> ProgramResult {
+    let account_ctx = CreateSessionV1Accounts::context(accounts)?;
+    create_session_v1(account_ctx, data, accounts)
+}
+
+fn process_create_sub_account_v1(accounts: &[AccountInfo], data: &[u8]) -> ProgramResult {
+    let account_ctx = CreateSubAccountV1Accounts::context(accounts)?;
+    create_sub_account_v1(account_ctx, data, accounts)
+}
+
+fn process_withdraw_from_sub_account_v1(
+    accounts: &[AccountInfo],
+    account_classification: &[AccountClassification],
+    data: &[u8],
+) -> ProgramResult {
+    let account_ctx = WithdrawFromSubAccountV1Accounts::context(accounts)?;
+    withdraw_from_sub_account_v1(account_ctx, accounts, data, account_classification)
+}
+
+fn process_sub_account_sign_v1(
+    accounts: &[AccountInfo],
+    account_classification: &[AccountClassification],
+    data: &[u8],
+) -> ProgramResult {
+    let account_ctx = SubAccountSignV1Accounts::context(accounts)?;
+    sub_account_sign_v1(account_ctx, accounts, data, account_classification)
+}
+
+fn process_toggle_sub_account_v1(accounts: &[AccountInfo], data: &[u8]) -> ProgramResult {
+    let account_ctx = ToggleSubAccountV1Accounts::context(accounts)?;
+    toggle_sub_account_v1(account_ctx, data, accounts)
 }
