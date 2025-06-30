e9049852b4326508b49358bef95e7be91be859c7
Tracy <31976538+tracy-codes@users.noreply.github.com>
2025-06-30 12:32:05 -0700

    Revert "CU fixes"
    This reverts commit 9d7ed56ea53cb3f9deac8ccb0968d7bb6fcd900a.

--- program/tests/sign_performance_test.rs
+++ program/tests/sign_performance_test.rs
@@ -188,9 +188,9 @@ fn test_token_transfer_performance_comparison() {
         "Account difference (swig - regular): {} accounts",
         account_difference
     );
-    // 4052 is the max difference in CU between the two transactions lets lower
+    // 3760 is the max difference in CU between the two transactions lets lower
     // this as far as possible but never increase it
-    assert!(swig_transfer_cu - regular_transfer_cu <= 4052);
+    assert!(swig_transfer_cu - regular_transfer_cu <= 3949);
 }
 
 #[test_log::test]
@@ -303,5 +303,5 @@ fn test_sol_transfer_performance_comparison() {
 
     // Set a reasonable limit for the CU difference to avoid regressions
     // Similar to the token transfer test assertion
-    assert!(swig_transfer_cu - regular_transfer_cu <= 2241);
+    assert!(swig_transfer_cu - regular_transfer_cu <= 2506);
 }
