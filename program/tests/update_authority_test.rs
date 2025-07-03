d6ebaf724b07c6d55c2716a5e1c33433aaeca7aa
Tracy Adams <31976538+tracy-codes@users.noreply.github.com>
2025-07-02 18:38:36 -0700

    simplify interface

--- program/tests/update_authority_test.rs
+++ program/tests/update_authority_test.rs
@@ -7,7 +7,9 @@ mod common;
 
 use common::*;
 use solana_sdk::{pubkey::Pubkey, signature::Keypair, signer::Signer};
-use swig_interface::{AuthorityConfig, ClientAction, UpdateAuthorityInstruction};
+use swig_interface::{
+    AuthorityConfig, ClientAction, UpdateAuthorityData, UpdateAuthorityInstruction,
+};
 use swig_state::{
     action::{all::All, manage_authority::ManageAuthority, sol_limit::SolLimit},
     authority::AuthorityType,
@@ -41,7 +43,7 @@ pub fn update_authority_with_ed25519_root(
         existing_ed25519_authority.pubkey(),
         role_id,
         authority_to_update_id,
-        new_actions,
+        UpdateAuthorityData::ReplaceAll(new_actions),
     )?;
 
     let msg = solana_sdk::message::v0::Message::try_compile(
@@ -58,7 +60,9 @@ pub fn update_authority_with_ed25519_root(
     )
     .map_err(|e| anyhow::anyhow!("Failed to create transaction {:?}", e))?;
 
-    let result = context.svm.send_transaction(tx)
+    let result = context
+        .svm
+        .send_transaction(tx)
         .map_err(|e| anyhow::anyhow!("Failed to send transaction {:?}", e))?;
 
     Ok(result)
@@ -67,13 +71,14 @@ pub fn update_authority_with_ed25519_root(
 #[test]
 fn test_update_authority_ed25519_replace_all() -> anyhow::Result<()> {
     let mut context = setup_test_context()?;
-    
+
     // Create initial wallet with Ed25519 authority
     let root_authority = Keypair::new();
     let id = [1u8; 32]; // Use a fixed ID for testing
     let (swig, _) = create_swig_ed25519(&mut context, &root_authority, id)?;
 
-    // Add a second authority that we can update (since we can't update root authority ID 0)
+    // Add a second authority that we can update (since we can't update root
+    // authority ID 0)
     let second_authority = Keypair::new();
     let second_authority_pubkey = second_authority.pubkey();
     let authority_config = AuthorityConfig {
@@ -81,7 +86,7 @@ fn test_update_authority_ed25519_replace_all() -> anyhow::Result<()> {
         authority: second_authority_pubkey.as_ref(),
     };
     let actions = vec![ClientAction::All(All {})];
-    
+
     let _add_result = add_authority_with_ed25519_root(
         &mut context,
         &swig,
@@ -107,14 +112,14 @@ fn test_update_authority_ed25519_replace_all() -> anyhow::Result<()> {
 
     // Verify the transaction succeeded by checking logs don't contain errors
     println!("Transaction logs: {:?}", result.logs);
-    
+
     Ok(())
 }
 
 #[test]
 fn test_update_authority_ed25519_add_actions() -> anyhow::Result<()> {
     let mut context = setup_test_context()?;
-    
+
     // Create initial wallet with Ed25519 authority
     let root_authority = Keypair::new();
     let id = [2u8; 32]; // Use a different ID for this test
@@ -128,7 +133,7 @@ fn test_update_authority_ed25519_add_actions() -> anyhow::Result<()> {
         authority: second_authority_pubkey.as_ref(),
     };
     let actions = vec![ClientAction::All(All {})];
-    
+
     let _add_result = add_authority_with_ed25519_root(
         &mut context,
         &swig,
@@ -147,18 +152,16 @@ fn test_update_authority_ed25519_add_actions() -> anyhow::Result<()> {
         .unwrap();
 
     // Add additional actions to the second authority (ID 1)
-    let additional_actions = vec![
-        ClientAction::SolLimit(SolLimit { amount: 500000 }),
-    ];
+    let additional_actions = vec![ClientAction::SolLimit(SolLimit { amount: 500000 })];
 
     // Use the add_actions method
-    let update_authority_ix = UpdateAuthorityInstruction::add_actions_with_ed25519_authority(
+    let update_authority_ix = UpdateAuthorityInstruction::new_with_ed25519_authority(
         swig,
         context.default_payer.pubkey(),
         root_authority.pubkey(),
         role_id,
         1, // authority_id 1 (the second authority)
-        additional_actions,
+        UpdateAuthorityData::AddActions(additional_actions),
     )?;
 
     let msg = solana_sdk::message::v0::Message::try_compile(
@@ -173,9 +176,11 @@ fn test_update_authority_ed25519_add_actions() -> anyhow::Result<()> {
         &[&context.default_payer, &root_authority],
     )?;
 
-    let result = context.svm.send_transaction(tx)
+    let result = context
+        .svm
+        .send_transaction(tx)
         .map_err(|e| anyhow::anyhow!("Failed to send transaction {:?}", e))?;
-    
+
     println!("Transaction logs: {:?}", result.logs);
 
     Ok(())
@@ -184,7 +189,7 @@ fn test_update_authority_ed25519_add_actions() -> anyhow::Result<()> {
 #[test]
 fn test_update_authority_ed25519_remove_by_type() -> anyhow::Result<()> {
     let mut context = setup_test_context()?;
-    
+
     // Create initial wallet with Ed25519 authority
     let root_authority = Keypair::new();
     let id = [3u8; 32]; // Use a different ID for this test
@@ -197,8 +202,11 @@ fn test_update_authority_ed25519_remove_by_type() -> anyhow::Result<()> {
         authority_type: AuthorityType::Ed25519,
         authority: second_authority_pubkey.as_ref(),
     };
-    let actions = vec![ClientAction::All(All {}), ClientAction::SolLimit(SolLimit { amount: 1000000 })];
-    
+    let actions = vec![
+        ClientAction::All(All {}),
+        ClientAction::SolLimit(SolLimit { amount: 1000000 }),
+    ];
+
     let _add_result = add_authority_with_ed25519_root(
         &mut context,
         &swig,
@@ -222,13 +230,13 @@ fn test_update_authority_ed25519_remove_by_type() -> anyhow::Result<()> {
     ];
 
     // Use the remove_by_type method on the second authority (ID 1)
-    let update_authority_ix = UpdateAuthorityInstruction::remove_actions_by_type_with_ed25519_authority(
+    let update_authority_ix = UpdateAuthorityInstruction::new_with_ed25519_authority(
         swig,
         context.default_payer.pubkey(),
         root_authority.pubkey(),
         role_id,
         1, // authority_id 1 (the second authority)
-        action_types_to_remove,
+        UpdateAuthorityData::RemoveActionsByType(action_types_to_remove),
     )?;
 
     let msg = solana_sdk::message::v0::Message::try_compile(
@@ -243,9 +251,11 @@ fn test_update_authority_ed25519_remove_by_type() -> anyhow::Result<()> {
         &[&context.default_payer, &root_authority],
     )?;
 
-    let result = context.svm.send_transaction(tx)
+    let result = context
+        .svm
+        .send_transaction(tx)
         .map_err(|e| anyhow::anyhow!("Failed to send transaction {:?}", e))?;
-    
+
     println!("Transaction logs: {:?}", result.logs);
 
     Ok(())
@@ -254,7 +264,7 @@ fn test_update_authority_ed25519_remove_by_type() -> anyhow::Result<()> {
 #[test]
 fn test_update_authority_ed25519_remove_by_index() -> anyhow::Result<()> {
     let mut context = setup_test_context()?;
-    
+
     // Create initial wallet with Ed25519 authority
     let root_authority = Keypair::new();
     let id = [4u8; 32]; // Use a different ID for this test
@@ -267,8 +277,11 @@ fn test_update_authority_ed25519_remove_by_index() -> anyhow::Result<()> {
         authority_type: AuthorityType::Ed25519,
         authority: second_authority_pubkey.as_ref(),
     };
-    let actions = vec![ClientAction::All(All {}), ClientAction::SolLimit(SolLimit { amount: 1000000 })];
-    
+    let actions = vec![
+        ClientAction::All(All {}),
+        ClientAction::SolLimit(SolLimit { amount: 1000000 }),
+    ];
+
     let _add_result = add_authority_with_ed25519_root(
         &mut context,
         &swig,
@@ -290,13 +303,13 @@ fn test_update_authority_ed25519_remove_by_index() -> anyhow::Result<()> {
     let indices_to_remove = vec![0u16]; // Remove first action
 
     // Use the remove_by_index method on the second authority (ID 1)
-    let update_authority_ix = UpdateAuthorityInstruction::remove_actions_by_index_with_ed25519_authority(
+    let update_authority_ix = UpdateAuthorityInstruction::new_with_ed25519_authority(
         swig,
         context.default_payer.pubkey(),
         root_authority.pubkey(),
         role_id,
         1, // authority_id 1 (the second authority)
-        indices_to_remove,
+        UpdateAuthorityData::RemoveActionsByIndex(indices_to_remove),
     )?;
 
     let msg = solana_sdk::message::v0::Message::try_compile(
@@ -311,40 +324,40 @@ fn test_update_authority_ed25519_remove_by_index() -> anyhow::Result<()> {
         &[&context.default_payer, &root_authority],
     )?;
 
-    let result = context.svm.send_transaction(tx)
+    let result = context
+        .svm
+        .send_transaction(tx)
         .map_err(|e| anyhow::anyhow!("Failed to send transaction {:?}", e))?;
-    
+
     println!("Transaction logs: {:?}", result.logs);
 
     Ok(())
 }
 
-// Basic tests for Secp256k1 and Secp256r1 - these would need proper signature generation
-// For now, just test that the instruction building works
+// Basic tests for Secp256k1 and Secp256r1 - these would need proper signature
+// generation For now, just test that the instruction building works
 
 #[test]
 fn test_update_authority_secp256k1_instruction_building() -> anyhow::Result<()> {
     let swig_pubkey = Pubkey::new_unique();
     let payer_pubkey = Pubkey::new_unique();
     let authority_pubkey = Pubkey::new_unique();
-    
-    let new_actions = vec![
-        ClientAction::All(All {}),
-    ];
+
+    let new_actions = vec![ClientAction::All(All {})];
 
     // Test that we can build Secp256k1 instructions without errors
     let _ix = UpdateAuthorityInstruction::new_with_secp256k1_authority(
         swig_pubkey,
         payer_pubkey,
         |_| [0u8; 65], // dummy signature function
-        0, // current_slot
-        0, // counter
-        0, // role_id
-        0, // authority_id
-        vec![ClientAction::All(All {})],
+        0,             // current_slot
+        0,             // counter
+        0,             // role_id
+        0,             // authority_id
+        UpdateAuthorityData::ReplaceAll(vec![ClientAction::All(All {})]),
     )?;
 
-    let _ix = UpdateAuthorityInstruction::add_actions_with_secp256k1_authority(
+    let _ix = UpdateAuthorityInstruction::new_with_secp256k1_authority(
         swig_pubkey,
         payer_pubkey,
         |_| [0u8; 65],
@@ -352,10 +365,21 @@ fn test_update_authority_secp256k1_instruction_building() -> anyhow::Result<()> 
         0, // counter
         0, // role_id
         0, // authority_id
-        vec![ClientAction::All(All {})],
+        UpdateAuthorityData::AddActions(vec![ClientAction::All(All {})]),
     )?;
 
-    let _ix = UpdateAuthorityInstruction::remove_actions_by_type_with_secp256k1_authority(
+    let _ix = UpdateAuthorityInstruction::new_with_secp256k1_authority(
+        swig_pubkey,
+        payer_pubkey,
+        |_| [0u8; 65],
+        0,                                                   // current_slot
+        0,                                                   // counter
+        0,                                                   // role_id
+        0,                                                   // authority_id
+        UpdateAuthorityData::RemoveActionsByType(vec![6u8]), // All action type discriminant
+    )?;
+
+    let _ix = UpdateAuthorityInstruction::new_with_secp256k1_authority(
         swig_pubkey,
         payer_pubkey,
         |_| [0u8; 65],
@@ -363,18 +387,7 @@ fn test_update_authority_secp256k1_instruction_building() -> anyhow::Result<()> 
         0, // counter
         0, // role_id
         0, // authority_id
-        vec![6u8], // All action type discriminant
-    )?;
-
-    let _ix = UpdateAuthorityInstruction::remove_actions_by_index_with_secp256k1_authority(
-        swig_pubkey,
-        payer_pubkey,
-        |_| [0u8; 65],
-        0, // current_slot
-        0, // counter
-        0, // role_id
-        0, // authority_id
-        vec![0u16],
+        UpdateAuthorityData::RemoveActionsByIndex(vec![0u16]),
     )?;
 
     Ok(())
@@ -385,10 +398,8 @@ fn test_update_authority_secp256r1_instruction_building() -> anyhow::Result<()> 
     let swig_pubkey = Pubkey::new_unique();
     let payer_pubkey = Pubkey::new_unique();
     let authority_pubkey = Pubkey::new_unique();
-    
-    let new_actions = vec![
-        ClientAction::All(All {}),
-    ];
+
+    let new_actions = vec![ClientAction::All(All {})];
 
     // Test that we can build Secp256r1 instructions without errors
     let dummy_pubkey = [0u8; 33]; // dummy public key
@@ -396,15 +407,15 @@ fn test_update_authority_secp256r1_instruction_building() -> anyhow::Result<()> 
         swig_pubkey,
         payer_pubkey,
         |_| [0u8; 64], // dummy signature function
-        0, // current_slot
-        0, // counter
-        0, // role_id
-        0, // authority_id
-        vec![ClientAction::All(All {})],
+        0,             // current_slot
+        0,             // counter
+        0,             // role_id
+        0,             // authority_id
+        UpdateAuthorityData::ReplaceAll(vec![ClientAction::All(All {})]),
         &dummy_pubkey,
     )?;
 
-    let _ix = UpdateAuthorityInstruction::add_actions_with_secp256r1_authority(
+    let _ix = UpdateAuthorityInstruction::new_with_secp256r1_authority(
         swig_pubkey,
         payer_pubkey,
         |_| [0u8; 64],
@@ -412,11 +423,23 @@ fn test_update_authority_secp256r1_instruction_building() -> anyhow::Result<()> 
         0, // counter
         0, // role_id
         0, // authority_id
-        vec![ClientAction::All(All {})],
+        UpdateAuthorityData::AddActions(vec![ClientAction::All(All {})]),
         &dummy_pubkey,
     )?;
 
-    let _ix = UpdateAuthorityInstruction::remove_actions_by_type_with_secp256r1_authority(
+    let _ix = UpdateAuthorityInstruction::new_with_secp256r1_authority(
+        swig_pubkey,
+        payer_pubkey,
+        |_| [0u8; 64],
+        0,                                                   // current_slot
+        0,                                                   // counter
+        0,                                                   // role_id
+        0,                                                   // authority_id
+        UpdateAuthorityData::RemoveActionsByType(vec![6u8]), // All action type discriminant
+        &dummy_pubkey,
+    )?;
+
+    let _ix = UpdateAuthorityInstruction::new_with_secp256r1_authority(
         swig_pubkey,
         payer_pubkey,
         |_| [0u8; 64],
@@ -424,19 +447,7 @@ fn test_update_authority_secp256r1_instruction_building() -> anyhow::Result<()> 
         0, // counter
         0, // role_id
         0, // authority_id
-        vec![6u8], // All action type discriminant
-        &dummy_pubkey,
-    )?;
-
-    let _ix = UpdateAuthorityInstruction::remove_actions_by_index_with_secp256r1_authority(
-        swig_pubkey,
-        payer_pubkey,
-        |_| [0u8; 64],
-        0, // current_slot
-        0, // counter
-        0, // role_id
-        0, // authority_id
-        vec![0u16],
+        UpdateAuthorityData::RemoveActionsByIndex(vec![0u16]),
         &dummy_pubkey,
     )?;
 
